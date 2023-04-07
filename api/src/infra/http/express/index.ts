df57a8145bdd946e7c756ed70548d0963b5f0f14
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-07 18:39:44 -0300

    feat: add files router

--- api/src/infra/http/express/index.ts
+++ api/src/infra/http/express/index.ts
@@ -1,9 +1,18 @@
 import express from 'express'
+import FilesRouter from "./routes/files.router";
 
 export default class App {
     public app
+    public filesRouter
 
     constructor() {
         this.app = express()
+        this.filesRouter = new FilesRouter().router
+
+        this.routes()
+    }
+
+    private routes(): void {
+        this.app.use('/files', this.filesRouter)
     }
 }
