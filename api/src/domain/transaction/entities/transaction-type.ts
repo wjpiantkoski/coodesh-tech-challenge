8c1c79d5440e1a860f3a4d569686e04b02e2bc9f
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-07 12:30:52 -0300

    refactor: refactor TransactionType entity
    Changed how id is generated

--- api/src/domain/transaction/entities/transaction-type.ts
+++ api/src/domain/transaction/entities/transaction-type.ts
@@ -1,42 +1,22 @@
-import UniqueEntityId from "../../@shared/value-object/unique-entity-id";
 import RequiredPropertyError from "../../@shared/errors/required-property.error";
 import InvalidPropertyError from "../../@shared/errors/invalid-property.error";
-
-export enum TransactionTypeNumber {
-    TypeOne = '1',
-    TypeTwo = '2',
-    TypeThree = '3',
-    TypeFour = '4'
-}
 
 export enum TransactionNature {
     CashIn = 'cash-in',
     CashOut = 'cash-out'
 }
-
-export type TransactionProps = {
-    typeNumber: TransactionTypeNumber,
-    description: string,
-    nature: TransactionNature
-}
 export default class TransactionType {
 
-    public readonly _id: UniqueEntityId
-    private _typeNumber: string
+    public readonly _id: string
     private _description: string
     private _nature: string
 
-    constructor(props: TransactionProps, id?: UniqueEntityId) {
-        this._id = id || new UniqueEntityId()
-        this._typeNumber = props.typeNumber
-        this._description = props.description
-        this._nature = props.nature
+    constructor(id: string, nature: string, description: string) {
+        this._id = id
+        this._nature = nature
+        this._description = description
 
         this.validate()
-    }
-
-    get typeNumber(): string {
-        return this._typeNumber
     }
 
     get description(): string {
