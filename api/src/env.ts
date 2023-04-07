d25d9da9e0973eda9b90c6346833405734309f22
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-07 19:03:39 -0300

    feat: add files router and database connection

--- api/src/env.ts
+++ api/src/env.ts
@@ -5,3 +5,9 @@ dotenvConfig()
 
 export const NODE_PATH = path.resolve(__dirname)
 export const SERVER_PORT = process.env.SERVER_PORT || 3000
+
+export const MYSQL_ROOT_PASSWORD = process.env.MYSQL_ROOT_PASSWORD
+export const MYSQL_DATABASE = process.env.MYSQL_DATABASE
+export const MYSQL_USER = process.env.MYSQL_USER
+export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
+export const MYSQL_HOST = process.env.MYSQL_HOST
