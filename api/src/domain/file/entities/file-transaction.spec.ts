b7f510a5e539593e1c8f6337c47cd9b782c05dd6
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-07 16:24:00 -0300

    feat: add FileTransacion validation method

--- api/src/domain/file/entities/file-transaction.spec.ts
+++ api/src/domain/file/entities/file-transaction.spec.ts
@@ -1,4 +1,6 @@
 import FileTransaction from "./file-transaction";
+import RequiredPropertyError from "../../@shared/errors/required-property.error";
+import InvalidPropertyError from "../../@shared/errors/invalid-property.error";
 
 describe('FileTransaction', function () {
 
@@ -18,4 +20,123 @@ describe('FileTransaction', function () {
         expect(fileTransaction.product).toBe('CURSO DE BEM-ESTAR            ')
     })
 
+    it('should throw an error when type is empty', () => {
+        expect(() => {
+            new FileTransaction(
+                '',
+                '2022-01-15T19:20:30-03:00',
+                'CURSO DE BEM-ESTAR            ',
+                '0000012750',
+                'JOSE CARLOS'
+            )
+        }).toThrow(RequiredPropertyError)
+    })
+
+    it('should throw an error when type is invalid', () => {
+        expect(() => {
+            new FileTransaction(
+                '12',
+                '2022-01-15T19:20:30-03:00',
+                'CURSO DE BEM-ESTAR            ',
+                '0000012750',
+                'JOSE CARLOS'
+            )
+        }).toThrow(InvalidPropertyError)
+    })
+
+    it('should throw an error when date is empty', () => {
+        expect(() => {
+            new FileTransaction(
+                '1',
+                '',
+                'CURSO DE BEM-ESTAR            ',
+                '0000012750',
+                'JOSE CARLOS'
+            )
+        }).toThrow(RequiredPropertyError)
+    })
+
+    it('should throw an error when date is invalid', () => {
+        expect(() => {
+            new FileTransaction(
+                '1',
+                '2022-01-15T19:20:30-03',
+                'CURSO DE BEM-ESTAR            ',
+                '0000012750',
+                'JOSE CARLOS'
+            )
+        }).toThrow(InvalidPropertyError)
+    })
+
+    it('should throw an error when value is empty', () => {
+        expect(() => {
+            new FileTransaction(
+                '1',
+                '2022-01-15T19:20:30-03:00',
+                'CURSO DE BEM-ESTAR            ',
+                '',
+                'JOSE CARLOS'
+            )
+        }).toThrow(RequiredPropertyError)
+    })
+
+    it('should throw an error when value is invalid', () => {
+        expect(() => {
+            new FileTransaction(
+                '1',
+                '2022-01-15T19:20:30-03:00',
+                'CURSO DE BEM-ESTAR            ',
+                '00000012750',
+                'JOSE CARLOS'
+            )
+        }).toThrow(InvalidPropertyError)
+    })
+
+    it('should throw an error when seller is empty', () => {
+        expect(() => {
+            new FileTransaction(
+                '1',
+                '2022-01-15T19:20:30-03:00',
+                'CURSO DE BEM-ESTAR            ',
+                '0000012750',
+                ''
+            )
+        }).toThrow(RequiredPropertyError)
+    })
+
+    it('should throw an error when seller is invalid', () => {
+        expect(() => {
+            new FileTransaction(
+                '1',
+                '2022-01-15T19:20:30-03:00',
+                'CURSO DE BEM-ESTAR            ',
+                '00000012750',
+                'JOSE CARLOS JOSE CARLOS JOSE CARLOS'
+            )
+        }).toThrow(InvalidPropertyError)
+    })
+
+    it('should throw an error when product is empty', () => {
+        expect(() => {
+            new FileTransaction(
+                '1',
+                '2022-01-15T19:20:30-03:00',
+                '',
+                '0000012750',
+                'JOSE CARLOS'
+            )
+        }).toThrow(RequiredPropertyError)
+    })
+
+    it('should throw an error when product is invalid', () => {
+        expect(() => {
+            new FileTransaction(
+                '1',
+                '2022-01-15T19:20:30-03:00',
+                'CURSO DE BEM-ESTAR  CURSO DE BEM-ESTAR          ',
+                '00000012750',
+                'JOSE CARLOS'
+            )
+        }).toThrow(InvalidPropertyError)
+    })
 });
--- api/src/domain/file/entities/file-transaction.ts
+++ api/src/domain/file/entities/file-transaction.ts
@@ -1,3 +1,6 @@
+import RequiredPropertyError from "../../@shared/errors/required-property.error";
+import InvalidPropertyError from "../../@shared/errors/invalid-property.error";
+
 export default class FileTransaction {
 
     private _type: string
@@ -12,6 +15,8 @@ export default class FileTransaction {
         this._value = value
         this._seller = seller
         this._product = product
+
+        this.validate()
     }
 
     get type(): string {
@@ -34,4 +39,35 @@ export default class FileTransaction {
         return this._product
     }
 
+    private validate(): void {
+        if (!this._type) {
+            throw new RequiredPropertyError('Type')
+        } else if (this._type.length > 1) {
+            throw new InvalidPropertyError('Type')
+        }
+
+        if (!this._date) {
+            throw new RequiredPropertyError('Date')
+        } else if (this._date.length !== 25) {
+            throw new InvalidPropertyError('Date')
+        }
+
+        if (!this._value) {
+            throw new RequiredPropertyError('Value')
+        } else if (this._value.length > 10) {
+            throw new InvalidPropertyError('Value')
+        }
+
+        if (!this._seller) {
+            throw new RequiredPropertyError('Seller')
+        } else if (this._seller.length > 20) {
+            throw new InvalidPropertyError('Seller')
+        }
+
+        if (!this._product) {
+            throw new RequiredPropertyError('Product')
+        } else if (this._product.length > 30) {
+            throw new InvalidPropertyError('Product')
+        }
+    }
 }
