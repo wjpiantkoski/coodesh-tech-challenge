144c2a7c3156ecd8976b0b8a8117eb57e339f8ca
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-07 14:40:21 -0300

    refactor: refactor populateTransactionType method

--- api/src/domain/transaction/services/transaction-type.service.ts
+++ api/src/domain/transaction/services/transaction-type.service.ts
@@ -1,9 +1,16 @@
-import TransactionType from "../entities/transaction-type";
+import TransactionType, {TransactionNature} from "../entities/transaction-type";
 import TransactionTypeRepository from "../../../infra/transaction/sequelize/repository/transaction-type.repository";
 
 export default class TransactionTypeService {
 
-    static async populateTransactionTypes(transactionTypes: TransactionType[]): Promise<void> {
+    static async populateTransactionTypes(): Promise<void> {
+        const transactionTypes = [
+            new TransactionType('1', TransactionNature.CashIn, 'Venda produtor'),
+            new TransactionType('2', TransactionNature.CashIn, 'Venda afiliado'),
+            new TransactionType('3', TransactionNature.CashOut, 'Comissão paga'),
+            new TransactionType('4', TransactionNature.CashIn, 'Comissão recebida'),
+        ]
+
         const transactionTypeRepository = new TransactionTypeRepository()
         await transactionTypeRepository.createMany(transactionTypes)
     }
