aea5b73dca98d9919b4176fa3bdbc8ae3f0723aa
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-08 17:10:49 -0300

    feat: update ListGroupedTRansactionsUseCase to calc transactions totals by seller

--- api/src/application/list-grouped-transactions.use-case.ts
+++ api/src/application/list-grouped-transactions.use-case.ts
@@ -1,5 +1,17 @@
 import TransactionRepository from "../infra/transaction/sequelize/repository/transaction.repository";
 import Transaction from "../domain/transaction/entities/transaction";
+import {group, sum} from 'radash'
+
+type TransactionListItem = {
+    seller: string,
+    total: number,
+    transactions: Transaction[]
+}
+
+type TransactionsList = {
+    items: TransactionListItem[],
+    total: number
+}
 
 export default class ListGroupedTransactionsUseCase {
 
@@ -9,9 +21,37 @@ export default class ListGroupedTransactionsUseCase {
         this._transactionRepository = transactionRepository
     }
 
-    async execute(): Promise<Transaction[]> {
+    async execute(): Promise<TransactionsList> {
         const transactions = await this._transactionRepository.findAll()
-        return transactions
+        const groupedTransactions = group(transactions, tr => tr.seller)
+
+        const sellers = Object.keys(groupedTransactions)
+
+        const transactionsList: TransactionsList = {
+            items: [],
+            total: 0
+        }
+
+        sellers.forEach(seller => {
+            const sellerItems = groupedTransactions[seller]
+
+            const total = sum(sellerItems, item => {
+                return item.type.nature === 'cash-out' ? item.value * -1 : item.value
+            })
+
+            transactionsList.items.push({
+                seller,
+                total,
+                transactions: sellerItems
+            })
+        })
+
+        transactionsList.total = sum(transactions, item => {
+            return item.type.nature === 'cash-out' ? item.value * -1 : item.value
+        })
+
+
+        return transactionsList
     }
 
 }
