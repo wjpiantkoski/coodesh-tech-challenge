651af515e7f698288dbfa94314f926a7aa59e202
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-08 16:20:08 -0300

    fix: update container files to wait database service start

--- api/src/env.ts
+++ api/src/env.ts
@@ -5,8 +5,6 @@ dotenvConfig()
 
 export const NODE_PATH = path.join(path.resolve(__dirname), '/uploads')
 export const SERVER_PORT = process.env.SERVER_PORT || 3000
-
-export const MYSQL_ROOT_PASSWORD = process.env.MYSQL_ROOT_PASSWORD
 export const MYSQL_DATABASE = process.env.MYSQL_DATABASE
 export const MYSQL_USER = process.env.MYSQL_USER
 export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
