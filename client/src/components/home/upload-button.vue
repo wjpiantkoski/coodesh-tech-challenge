ff38809a86331cc4af23dd5cbe3d175a467cc856
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-09 20:47:48 -0300

    feat: add file upload button

---
+++ client/src/components/home/upload-button.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'UploadButton'
+}
+</script>
+
+<template>
+  <v-btn
+    fab
+    right
+    bottom
+    absolute
+    color="primary"
+    class="mb-16 mr-5"
+  >
+    <v-icon>mdi-file-upload</v-icon>
+  </v-btn>
+</template>
--- client/src/views/home-view.vue
+++ client/src/views/home-view.vue
@@ -1,14 +1,24 @@
 <script>
 import NavigationBar from "@/components/home/navigation-bar.vue";
+import UploadButton from "@/components/home/upload-button.vue";
 
 export default {
   name: 'HomeView',
-  components: {NavigationBar}
+  components: {UploadButton, NavigationBar}
 }
 </script>
 
 <template>
   <v-main>
+    <upload-button/>
     <navigation-bar/>
+
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs12 class="text-right">
+
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-main>
 </template>
