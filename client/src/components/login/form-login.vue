a6e556896f53502db78b6dbab361cd055ed16319
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-09 20:15:32 -0300

    feat: implement login with firebase authentication

--- client/src/components/login/form-login.vue
+++ client/src/components/login/form-login.vue
@@ -1,4 +1,7 @@
 <script>
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth"
+import {email, required} from 'vuelidate/lib/validators'
+
 export default {
   name: 'FormLogin',
   data: () => ({
@@ -7,9 +10,55 @@ export default {
     password: '',
     showPassword: ''
   }),
+  validations: {
+    email: {
+      email,
+      required
+    },
+    password: {
+      required
+    }
+  },
+  computed: {
+    emailErrors() {
+      if (!this.$v.email.$anyDirty) {
+        return []
+      }
+
+      if (this.$v.email.$anyError) {
+        return ['Preencha um e-mail válido']
+      }
+    },
+    passwordErrors() {
+      if (!this.$v.password.$anyDirty) {
+        return []
+      }
+
+      if (this.$v.password.$anyError) {
+        return ['Preencha uma senha válida']
+      }
+    }
+  },
   methods: {
     async submit() {
+      try {
+        this.$v.$touch()
 
+        if (!this.$v.$anyError) {
+          this.loader = true
+
+          const email = this.email.trim().toLowerCase()
+
+          await signInWithEmailAndPassword(getAuth(), email, this.password)
+          await this.$router.push('/')
+        }
+      } catch (err) {
+        if (process.env.NODE_ENV !== 'production') {
+          console.error(err)
+        }
+      } finally {
+        this.loader = false
+      }
     }
   }
 }
@@ -29,6 +78,7 @@ export default {
             type="email"
             label="E-mail"
             v-model="email"
+            :error-messages="emailErrors"
         ></v-text-field>
 
         <v-text-field
@@ -36,6 +86,7 @@ export default {
             label="Senha"
             type="password"
             v-model="password"
+            :error-messages="passwordErrors"
         ></v-text-field>
 
         <div>
