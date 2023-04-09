aacba8ef31270b1a2244970d77dc320980888a40
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-09 18:11:55 -0300

    chore: add firebase setup

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,24 @@
-import {createApp} from 'vue'
+import {createApp, App as VueApp} from 'vue'
+import {getAuth} from 'firebase/auth'
+
 import './style.css'
 import App from './App.vue'
 
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import {firebaseApp} from "./plugins/firebaseApp"
 
-createApp(App).use(vuetify).use(router).mount('#app')
+let app: VueApp
+
+const auth = getAuth(firebaseApp)
+
+auth.onAuthStateChanged(async () => {
+    if (!app) {
+        app = createApp(App);
+
+        app.use(vuetify)
+        app.use(router)
+
+        app.mount("#app");
+    }
+})
