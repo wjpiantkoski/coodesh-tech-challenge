63ec8732debb2de0dcffcf79d23daf532f23f4fb
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-09 18:20:42 -0300

    feat: add home view

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,18 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import LoginView from "../views/login-view.vue";
+import HomeView from "../views/home-view.vue";
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: HomeView,
+            meta: {
+                requiresAuth: true
+            }
+        },
         {
             path: '/login',
             name: 'Login',
