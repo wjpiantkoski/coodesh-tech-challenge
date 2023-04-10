97044a2016bfa874f70fd81ff45c8af5e5c2ff6a
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-09 22:43:40 -0300

    feat: add seller transactions table

--- client/src/components/home/card-seller-transactions.vue
+++ client/src/components/home/card-seller-transactions.vue
@@ -1,12 +1,58 @@
 <script>
 import numberToCurrency from "@/utils/numberToCurrency";
+import dayjs from "@/utils/dayjs";
 
 export default {
   name: 'CardSellerTransactions',
   props: ['sellerData'],
+  data: () => ({
+    tableHeaders: [
+      {
+        text: 'Produto',
+        align: 'start',
+        sortable: true,
+        value: '_product'
+      },
+      {
+        text: 'Data',
+        align: 'center',
+        sortable: true,
+        value: '_date'
+      },
+      {
+        text: 'Tipo',
+        align: 'start',
+        sortable: true,
+        value: '_type._description'
+      },
+      {
+        text: 'Valor',
+        align: 'start',
+        sortable: true,
+        value: '_value'
+      },
+    ]
+  }),
   computed: {
     total() {
       return numberToCurrency(this.sellerData.total)
+    }
+  },
+  methods: {
+    formatValue(valueCents, transactionNature) {
+      if (valueCents) {
+        let total = transactionNature === 'cash-out' ? valueCents * -1 : valueCents
+        return numberToCurrency(total / 100)
+      }
+
+      return ''
+    },
+    formatDate(date) {
+      if (date) {
+        return dayjs(date).format('DD/MM/YYYY')
+      }
+
+      return ''
     }
   }
 }
@@ -26,7 +72,23 @@ export default {
     </v-card-title>
 
     <v-card-text>
+      <v-data-table
+          :headers="tableHeaders"
+          :items="sellerData.transactions"
+      >
+        <template v-slot:item._date="{ item }">
+          {{ formatDate(item._date) }}
+        </template>
 
+        <template v-slot:item._value="{ item }">
+          <v-chip
+              dark
+              :color="item._type._nature === 'cash-out' ? 'error' : 'success'"
+          >
+            {{ formatValue(item._value, item._type._nature) }}
+          </v-chip>
+        </template>
+      </v-data-table>
     </v-card-text>
   </v-card>
 </template>
